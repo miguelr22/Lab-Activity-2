8dbd77c476f1bb498835d73fe3cef8a9d7ef09d5
miguelr22 <72761857+miguelr22@users.noreply.github.com>
2021-03-04 11:50:35 -0600

    Add files via upload

---
+++ Miguel Rodriguez CSS 225 Module 2 Lab Activity Problem 1.py
@@ -0,0 +1,5 @@
+def say_hello():
+    print("Hello, Welcome!")
+
+#What is your name?
+#Hello Marco, Hope You Have A Good Day.
---
+++ Miguel Rodriguez CSS 225 Module 2 Lab Activity Problem 2.py
@@ -0,0 +1,5 @@
+def bmi():
+    # What is your height and weight?
+    # My height is 5 feet 11 inches
+    # I weigh about 180 pounds.
+#Weight in lbs/ height in inches squared (703)
